6bcf14506a948f22bf525efbdca68b15b6dd3261
Tirinor24 <87747790+Tirinor24@users.noreply.github.com>
2023-07-07 01:24:11 +0300

    Update Calculator.java

--- TestCalc/src/Calculator.java
+++ TestCalc/src/Calculator.java
@@ -14,16 +14,16 @@ class Main {
     private static int b;
     private static int result;
     private static boolean isRoman;
-    private static String[] romanNumerals = {“I”, “II”, “III”, “IV”, “V”, “VI”, “VII”, “VIII”, “IX”, “X”,
-            “XI”, “XII”, “XIII”, “XIV”, “XV”, “XVI”, “XVII”, “XVIII”, “XIX”, “XX”,
-            “XXI”, “XXII”, “XXIII”, “XXIV”, “XXV”, “XXVI”, “XXVII”, “XXVIII”, “XXIX”, “XXX”,
-            “XXXI”, “XXXII”, “XXXIII”, “XXXIV”, “XXXV”, “XXXVI”, “XXXVII”, “XXXVIII”, “XXXIX”, “XXXX”,
-            “XLI”, “XLII”, “XLIII”, “XLIV”, “XLV”, “XLVI”, “XLVII”, “XLVIII”, “XLIX”, “L”,
-            “LI”, “LII”, “LIII”, “LIV”, “LV”, “LVI”, “LVII”, “LVIII”, “LIX”, “LX”,
-            “LXI”, “LXII”, “LXIII”, “LXIV”, “LXV”, “LXVI”, “LXVII”, “LXVIII”, “LXIX”, “LXX”,
-            “LXXI”, “LXXII”, “LXXIII”, “LXXIV”, “LXXV”, “LXXVI”, “LXXVII”, “LXXVIII”, “LXXIX”, “LXXX”,
-            “LXXXI”, “LXXXII”, “LXXXIII”, “LXXXIV”, “LXXXV”, “LXXXVI”, “LXXXVII”, “LXXXVIII”, “LXXXIX”, “XC”,
-            “XCI”, “XCII”, “XCIII”, “XCIV”, “XCV”, “XCVI”, “XCVII”, “XCVIII”, “XCIX”, “C”};
+    private static String[] romanNumerals = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+            "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+            "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XXXX",
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+            "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
       public static String calc(String input) throws Exception {
         String[] elements = input.split(" ");
         if (elements.length != 3) {
